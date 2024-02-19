6189f11711938fcb850da028d5e999f53925ec08
deformal <drake9211@gmail.com>
2024-02-20 00:29:02 +0530

    setup eslint and prettier
    Features
    
    Fixes
    
    Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { createSignal } from 'solid-js';
+import { createSignal } from "solid-js";
 
 function App() {
-	const [count, setCount] = createSignal(0);
+  const [count, setCount] = createSignal(0);
 
-	return (
-		<>
-			<div>
-				<h1>Hello</h1>
-			</div>
-			<h1>Vite + Solid</h1>
-			<div class="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count()}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p class="read-the-docs">
-				Click on the Vite and Solid logos to learn more
-			</p>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <h1>Vite + Solid</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
+    </>
+  );
 }
 
 export default App;
